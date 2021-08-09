6d6e906c63af44b48b54e444d485bcf33156acdb
Ivan Rodriguez <irodriguez@lean-tech.io>
2021-08-08 22:53:40 -0500

    configuration to deploy on heroku

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const PORT = process.env.PORT || 5000;
-
   app.enableCors();
-  await app.listen(PORT);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
