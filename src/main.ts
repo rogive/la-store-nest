6db7e9e754ccf7c04ea86cfdb45d15522a223eca
Ivan Rodriguez <irodriguez@lean-tech.io>
2021-08-08 19:32:14 -0500

    initial config

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const PORT = process.env.PORT || 5000;
+
+  app.enableCors();
+  await app.listen(PORT);
+}
+bootstrap();
---
+++ src/product/product.controller.ts
@@ -0,0 +1,73 @@
+import {
+  Controller,
+  Res,
+  HttpStatus,
+  Param,
+  Post,
+  Body,
+  Get,
+  Put,
+  Delete,
+  NotFoundException,
+} from '@nestjs/common';
+
+import { ProductDTO } from './dto/product.dto';
+import { ProductService } from './product.service';
+
+@Controller('product')
+export class ProductController {
+  constructor(private productService: ProductService) {}
+
+  @Post('/')
+  async create(@Res() res, @Body() body: ProductDTO) {
+    const product = this.productService.create(body);
+
+    return res.status(HttpStatus.OK).json({
+      message: 'Product created succesfully',
+      product,
+    });
+  }
+
+  @Get('/list')
+  async getProducts(@Res() res) {
+    const products = await this.productService.getProducts();
+    return res.status(HttpStatus.OK).json({
+      message: 'successful',
+      products,
+    });
+  }
+
+  @Get('/:id')
+  async getProduct(@Res() res, @Param('id') productID) {
+    const product = await this.productService.getProduct(productID);
+    if (!product) throw new NotFoundException('Product not found');
+    return res.status(HttpStatus.OK).json({
+      message: 'successful',
+      product,
+    });
+  }
+
+  @Delete('/:id')
+  async deleteProduct(@Res() res, @Param('id') productID) {
+    const product = await this.productService.deleteProduct(productID);
+    if (!product) throw new NotFoundException('Product not found');
+    return res.status(HttpStatus.OK).json({
+      message: 'Product deleted succesfully',
+      product,
+    });
+  }
+
+  @Put('/:id')
+  async updateProduct(
+    @Res() res,
+    @Param('id') productID,
+    @Body() body: ProductDTO,
+  ) {
+    const product = await this.productService.updateProduct(productID, body);
+    if (!product) throw new NotFoundException('Product not found');
+    return res.status(HttpStatus.OK).json({
+      message: 'Product updated succesfully',
+      product,
+    });
+  }
+}
